0b87ee996c1a1b0ced3ab82b914acc4208813950
wakanaowl <you@example.com>
2022-03-08 03:10:59 +0900

    first

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 @app.route('/')
 def index():
     return "Hello, World!!!"
+
+#追加
+@app.route('/json',methods=['POST'])
+def json():
+    #受け取ったjsonをそのまま返す
+    json = request.json
+    return json
 
 if __name__ == '__main__':
     #app.run(host='0.0.0.0',port=5000)
